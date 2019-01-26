bfa4c72ca50e48e36a9177497ba4eec9bbb645d7
Masaharu Dobashi <dobashi.masaharu@gmail.com>
2019-01-26 22:43:03 +0900

    問題を作成する画面のファイルを作成

---
+++ Quiz/View/QuizCreateView/QuizCreateView.swift
@@ -0,0 +1,21 @@
+//
+//  QuizCreateView.swift
+//  Quiz
+//
+//  Created by 土橋正晴 on 2019/01/26.
+//  Copyright © 2019 m.dobashi. All rights reserved.
+//
+
+import UIKit
+
+class QuizCreateView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
