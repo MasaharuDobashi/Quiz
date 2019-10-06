4591f805a52482118527f2c0894aeed267cd2822
Masaharu Dobashi <dobashi.masaharu@gmail.com>
2019-10-06 22:56:45 +0900

    ダークモード時の色を変更する関数を追加

--- Quiz/Other/Constant.swift
+++ Quiz/Other/Constant.swift
@@ -17,12 +17,13 @@ let realmConfig: UInt64 = 1
 
 // MARK: - COLORS
 
-let Morganite:UIColor = UIColor.rgba(red: 205, green: 192, blue: 199, alpha: 1)
-let Dawnpink:UIColor = UIColor.rgba(red: 208, green: 184, blue: 187, alpha: 1)
-let Geranium:UIColor = UIColor.rgba(red: 218, green: 61, blue: 92, alpha: 1)
-let Rubyred:UIColor = UIColor.rgba(red: 207, green: 53, blue: 93, alpha: 1)
-let Beige: UIColor = UIColor.rgba(red: 245, green: 245, blue: 220, alpha: 1)
-let Rose:UIColor = UIColor.rgba(red: 244, green: 80, blue: 109, alpha: 1)
+
+let Morganite:UIColor = UIColor.changColor(light: UIColor.rgba(red: 205, green: 192, blue: 199, alpha: 1), dark: .black)
+let Dawnpink:UIColor = UIColor.changColor(light: UIColor.rgba(red: 208, green: 184, blue: 187, alpha: 1), dark: .black)
+let Geranium:UIColor = UIColor.changColor(light: UIColor.rgba(red: 218, green: 61, blue: 92, alpha: 1), dark: .black)
+let Rubyred:UIColor = UIColor.changColor(light: UIColor.rgba(red: 207, green: 53, blue: 93, alpha: 1), dark: .black)
+let Beige: UIColor = UIColor.changColor(light: UIColor.rgba(red: 245, green: 245, blue: 220, alpha: 1), dark: .black)
+let Rose:UIColor = UIColor.changColor(light: UIColor.rgba(red: 244, green: 80, blue: 109, alpha: 1), dark: .black)
 
 
 
