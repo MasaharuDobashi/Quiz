edc9b11658ea1184e76d9cc6bb1a5c3fb785c2f5
Masaharu Dobashi <dobashi.masaharu@gmail.com>
2019-10-06 23:02:11 +0900

    リネーム漏れ修正

--- Quiz/Other/Constant.swift
+++ Quiz/Other/Constant.swift
@@ -18,12 +18,12 @@ let realmConfig: UInt64 = 1
 // MARK: - COLORS
 
 
-let Morganite:UIColor = UIColor.changColor(light: UIColor.rgba(red: 205, green: 192, blue: 199, alpha: 1), dark: .black)
-let Dawnpink:UIColor = UIColor.changColor(light: UIColor.rgba(red: 208, green: 184, blue: 187, alpha: 1), dark: .black)
-let Geranium:UIColor = UIColor.changColor(light: UIColor.rgba(red: 218, green: 61, blue: 92, alpha: 1), dark: .black)
-let Rubyred:UIColor = UIColor.changColor(light: UIColor.rgba(red: 207, green: 53, blue: 93, alpha: 1), dark: .black)
-let Beige: UIColor = UIColor.changColor(light: UIColor.rgba(red: 245, green: 245, blue: 220, alpha: 1), dark: .black)
-let Rose:UIColor = UIColor.changColor(light: UIColor.rgba(red: 244, green: 80, blue: 109, alpha: 1), dark: .black)
+let Morganite:UIColor = UIColor.changeAppearnceColor(light: UIColor.rgba(red: 205, green: 192, blue: 199, alpha: 1), dark: .black)
+let Dawnpink:UIColor = UIColor.changeAppearnceColor(light: UIColor.rgba(red: 208, green: 184, blue: 187, alpha: 1), dark: .black)
+let Geranium:UIColor = UIColor.changeAppearnceColor(light: UIColor.rgba(red: 218, green: 61, blue: 92, alpha: 1), dark: .black)
+let Rubyred:UIColor = UIColor.changeAppearnceColor(light: UIColor.rgba(red: 207, green: 53, blue: 93, alpha: 1), dark: .black)
+let Beige: UIColor = UIColor.changeAppearnceColor(light: UIColor.rgba(red: 245, green: 245, blue: 220, alpha: 1), dark: .gray)
+let Rose:UIColor = UIColor.changeAppearnceColor(light: UIColor.rgba(red: 244, green: 80, blue: 109, alpha: 1), dark: .black)
 
 
 
