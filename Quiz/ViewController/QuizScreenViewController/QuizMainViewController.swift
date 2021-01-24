5b3e360646c143f201d5b7700da94b659dab4d73
MasaharuDobashi <dobashi.masaharu@gmail.com>
2021-01-24 21:33:49 +0900

    QuizMainViewControllerのリファクタリング

--- Quiz/ViewController/QuizScreenViewController/QuizMainViewController.swift
+++ Quiz/ViewController/QuizScreenViewController/QuizMainViewController.swift
@@ -11,31 +11,23 @@ import UIKit
 // MARK: - QuizMainViewController
 
 
-class QuizMainViewController: UIViewController, QuizMainViewDelegate {
+final class QuizMainViewController: UIViewController {
     
     // MARK: Properties
 
     /// スタートボタンや履歴ボタンを表示する画面
-    private lazy var quizMainView:QuizMainView = {
-        let quizMainView: QuizMainView = QuizMainView(frame: frame_Size(self))
-        quizMainView.delegate = self
-        
-        return quizMainView
-    }()
-    
+    private var quizMainView: QuizMainView!
     
     
     // MARK: Lifecycle
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setQuizMainView()
         setNotificationCenter()
-        view.addSubview(quizMainView)
     }
     
-    
+
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -45,39 +37,13 @@ class QuizMainViewController: UIViewController, QuizMainViewDelegate {
         quizMainView.isHistory = HistoryModel.allFindHistory(self).isEmpty ? false : true
     }
     
+  
     
-    // MARK: QuizMainViewDelegate
-    
-    
-    /// クイズがあればクイズをスタートする、なければクイズの作成モーダルを表示する
-    func quizStartButtonAction() {
-        if quizMainView.isActiveQuiz {
-            presentModalView(QuizScreenViewController())
-            
-        } else {
-            presentModalView(QuizEditViewController(mode: .add))
-            
-        }
+    func setQuizMainView() {
+        quizMainView = QuizMainView(frame: UIScreen.main.bounds)
+        quizMainView.delegate = self
+        view.addSubview(quizMainView)
     }
-    
-    
-    /// クイズのカテゴリがあれば選択画面に遷移する、なければクイズのカテゴリを作成モーダルを表示する
-    func quizTypeButtonAction() {
-        if quizMainView.isQuizType {
-            pushTransition(QuizTypeSelectTableViewController())
-            
-        } else {
-            presentModalView(QuizTypeEditViewController(typeid: nil, createTime: nil, mode: .add))
-            
-        }
-    }
-    
-    /// 履歴画面を開く
-    func historyButtonAction() {
-        pushTransition(HistoryViewController())
-    }
-    
-    
     
     
     /// addObserverをセットする
@@ -86,7 +52,6 @@ class QuizMainViewController: UIViewController, QuizMainViewDelegate {
         /// QuizManagementViewControllerでallDeleteがpostされたら履歴ボタンを更新する
         NotificationCenter.default.addObserver(self, selector: #selector(allDeleteFlag(notification:)), name: NSNotification.Name(rawValue: R.string.notifications.allDelete()), object: nil)
         #endif
-        
         
         /// R.notification.ViewUpdate、R.notification.QuizUpdateがpostされたらViewWillAppearを呼ぶ
         NotificationCenter.default.addObserver(self, selector: #selector(callViewWillAppear(notification:)), name: NSNotification.Name(rawValue: R.string.notifications.quizUpdate()), object: nil)
@@ -117,3 +82,33 @@ class QuizMainViewController: UIViewController, QuizMainViewDelegate {
     
 }
 
+
+
+// MARK: - QuizMainViewDelegate
+
+extension QuizMainViewController: QuizMainViewDelegate {
+        
+    /// クイズがあればクイズをスタートする、なければクイズの作成モーダルを表示する
+    func quizStartButtonAction() {
+        if quizMainView.isActiveQuiz {
+            presentModalView(QuizScreenViewController())
+        } else {
+            presentModalView(QuizEditViewController(mode: .add))
+        }
+    }
+    
+    
+    /// クイズのカテゴリがあれば選択画面に遷移する、なければクイズのカテゴリを作成モーダルを表示する
+    func quizTypeButtonAction() {
+        if quizMainView.isQuizType {
+            pushTransition(QuizTypeSelectTableViewController())
+        } else {
+            presentModalView(QuizTypeEditViewController(typeid: nil, createTime: nil, mode: .add))
+        }
+    }
+    
+    /// 履歴画面を開く
+    func historyButtonAction() {
+        pushTransition(HistoryViewController())
+    }
+}
